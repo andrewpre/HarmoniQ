9e90660b8c9c0f9c6b89e5ef455a6d8548d49f22
Andrew Presman <alpresma@buffalo.edu>
2025-01-27 22:00:30 -0500

    init

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -1,11 +1,11 @@
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users_table', {
-    id: text('id').primaryKey(),
-    name: text('name').notNull(),
-    email: text('email').notNull().unique(),
-    plan: text('plan').notNull(),
-    stripe_id: text('stripe_id').notNull(),
+  id: text('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  plan: text('plan').notNull(),
+  stripe_id: text('stripe_id').notNull(),
 });
 
 export type InsertUser = typeof usersTable.$inferInsert;
